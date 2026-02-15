7ea9420554a1dc3a2f155d183ba37ac8fd78d62e
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2026-02-15 14:42:25 +0000

    Variables testing

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,13 @@
+const account_Id  = 14453 
+let acoountEmail = "ishitabairagi200@gmail.com"
+var accountPassword = "ishita"
+accountCity = "Jaipur"
+
+//constant = const , ye change nahi hona chaiye 
+// accountEmail = "fhgedjkews@gmail.com"
+// accountPassword = "12121212"
+// accountCity = "BHopal"
+// console.log(account_Id);
+/* Perfer not to use var , beacuse of issue in block scope and functional scope */
+
+console.table([account_Id, accountEmail,accountPassword, accountCity])
