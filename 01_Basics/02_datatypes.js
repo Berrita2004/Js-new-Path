164bb884e882f8c328431ff554197af495d69ee3
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2026-02-16 06:16:29 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all js CODE as Newer Ver
+console.log(3 + 3)
+console.log("name ")
+
+let name = "priya"
+let age = 98
+let isLoggedIn = false 
+let state = null
+
+
+
+console.log (typeof "priya")
+console.log (typeof null)// output = null
+console.log (typeof undefined) // output = undefined 
