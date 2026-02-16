4b46e3b38eda440b88dd21187fd711b04257aa74
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2026-02-16 06:17:20 +0000

    some notes added

--- 01_Basics/02_datatypes.js
+++ 01_Basics/02_datatypes.js
@@ -7,7 +7,14 @@ let age = 98
 let isLoggedIn = false 
 let state = null
 
-
+//number -> 2 to power 53
+//bigint , stockmarket 
+//string -> " string whatever "
+//boolean - true / false 
+// null -> standalone Value , representation of an empty value 
+//undefined -> a value that is not yet defined 
+//symbol    -> mostly on react unique 
+//obejct 
 
 console.log (typeof "priya")
 console.log (typeof null)// output = null
