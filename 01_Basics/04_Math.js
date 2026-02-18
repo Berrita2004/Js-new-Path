a5d761ab8fcc45964d7976528938642d994c4963
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2026-02-18 10:28:52 +0000

    add Math.js with examples of mathematical functions and random number generation

---
+++ 01_Basics/04_Math.js
@@ -0,0 +1,14 @@
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+let num = 4.7
+console.log(Math.round(num));
+console.log(Math.floor(num));
+console.log(Math.ceil(num));
+console.log(Math.trunc(num));
+
+let randomNum = Math.random()
+console.log(randomNum);
+let randomNumBetween1And10 = Math.floor(Math.random() * 10) + 1
+console.log(randomNumBetween1And10);
