bd32cd99f955c18cf2d34c6cd95fbb334e4c7734
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2026-03-03 07:13:14 +0000

    refactor arrays.js to comment out unused code and enhance clarity

--- 02_Basics/arrays.js
+++ 02_Basics/arrays.js
@@ -21,12 +21,35 @@
 //arry mutable in js   
 // diffrent arrays 
 
-const markets = ["D-Mart", "Reliance", "Big Bazaar"];
-const fruits = ["Apple", "Banana", "Mango"];
+// const markets = ["D-Mart", "Reliance", "Big Bazaar"];
+// const fruits = ["Apple", "Banana", "Mango"];
 // markets.push(fruits);
 // console.log(markets[3][2]); // Accessing "Mango" from the nested array
 
 
 // concat the arrays
-const combinedArray = markets.concat(fruits);
-console.log(combinedArray);
+// const combinedArray = markets.concat(fruits);
+// console.log(combinedArray);
+
+// // using spread operator to combine arrays
+// const combinedArrayWithSpread = [...markets, ...fruits];
+// console.log(combinedArrayWithSpread);       
+
+//multidimensional array    
+const multiDimensionalArray = [1,2,3,[4,5,6],7,[8,9,[10,11,12]]];
+console.log(multiDimensionalArray[2][1]); // Accessing 5 from the nested array
+const nestedArray = multiDimensionalArray.flat(Infinity);
+console.log(nestedArray); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] 
+
+
+//scraping data from website using arrays
+console.log(Array.isArray("Ishita"));
+console.log(Array.from("Ishita"));
+console.log(Array.from({name: "Ishita"})); //interesting case, it will create an array of undefined values with length equal to the number of properties in the object
+
+let score1 =100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3)); // Output: [100, 200, 300]
+// console.log(Array.of(...scores));
